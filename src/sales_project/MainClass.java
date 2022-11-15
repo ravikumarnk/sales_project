a61e5edbea22c38d5bfb8fa781aeed4e9aee7197
ravikumar <ravikelakam@gmail.com>
2022-11-15 10:48:54 +0530

    This is project base model. created by team lead ..

---
+++ src/sales_project/MainClass.java
@@ -0,0 +1,18 @@
+package sales_project;
+
+public class MainClass {
+
+	public MainClass() {
+	
+		
+		System.out.println("here is the master project.. with configurations.. ");
+		
+	}
+	
+	
+	
+	
+	
+}
+
+
