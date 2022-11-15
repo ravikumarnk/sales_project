a28ef5dc6638b8b8d7063edc27475116e72c2d1c
ravikumar <ravikelakam@gmail.com>
2022-11-15 11:00:47 +0530

    committed with second line

--- src/sales_project/MainClass.java
+++ src/sales_project/MainClass.java
@@ -3,9 +3,8 @@ package sales_project;
 public class MainClass {
 
 	public MainClass() {
-	
-		
 		System.out.println("here is the master project.. with configurations.. ");
+		System.out.println("will add methods under this   ..  ");
 		
 	}
 	
